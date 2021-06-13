5216bcfcfeca843cc9db3af0e824d78958dfd596
Siwat Laothong <bluehunt.kho@gmail.com>
2021-06-13 20:28:03 +0700

    initail commit

---
+++ android/app/src/main/kotlin/com/pondwickproduct/market_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pondwickproduct.market_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/restaurants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Restaurant {
+  final String id;
+  final String title;
+  final int mobile;
+  final String image;
+
+  Restaurant({
+    @required this.id,
+    @required this.title,
+    @required this.mobile,
+    @required this.image,
+  });
+}
