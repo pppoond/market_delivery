d445ccfea131feaff2ee5bc0c4283f388870a932
pppoond <bluehunt.kho@gmail.com>
2021-06-14 21:09:42 +0700

    master

---
+++ lib/model/menu.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class MenuItem {
+  final String id;
+  final double price;
+  final String title;
+  final String image;
+  final String restaurantTitle;
+  final String restaurantId;
+
+  MenuItem({
+    @required this.id,
+    @required this.title,
+    @required this.price,
+    @required this.image,
+    @required this.restaurantTitle,
+    @required this.restaurantId,
+  });
+}
--- lib/model/restaurants.dart
+++ lib/model/restaurants.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import './menu.dart';
 
 class Restaurant {
   final String id;
   final String title;
   final int mobile;
   final String image;
+  List<MenuItem> menu;
 
   Restaurant({
     @required this.id,
     @required this.title,
     @required this.mobile,
     @required this.image,
+    @required this.menu,
   });
 }
-//sadasd
