2e5989bc75254416acefa61555805b28a7b4e5d3
pppoond <bluehunt.kho@gmail.com>
2021-08-05 00:18:38 +0700

    pppoond

--- lib/widgets/menu_list_item.dart
+++ lib/widgets/menu_list_item.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../screens/detail_screen.dart';
+
 class MenuListItem extends StatelessWidget {
   final String menuId;
   final String menuTitle;
@@ -23,34 +25,91 @@ class MenuListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _deviceSize = MediaQuery.of(context).size;
-    return Container(
-      width: _deviceSize.width * 0.45,
-      height: _deviceSize.height * 0.20,
-      child: Card(
-        child: Column(
-          children: [
-            Container(
-              width: _deviceSize.width * 0.50,
-              height: _deviceSize.height * 0.20,
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(5),
-                  topRight: Radius.circular(5),
-                ),
-                child: Image.network(
-                  menuImage,
-                  fit: BoxFit.cover,
+    double _discount = (menuPrice * 150) / 100;
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(DetailScreen.routeName, arguments: menuRestaurantId);
+      },
+      child: Container(
+        width: _deviceSize.width * 0.45,
+        height: _deviceSize.height * 0.20,
+        child: Card(
+          key: ValueKey(menuId),
+          elevation: 0.5,
+          shadowColor: Colors.grey.shade200,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: _deviceSize.width * 0.50,
+                height: _deviceSize.height * 0.20,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(5),
+                    topRight: Radius.circular(5),
+                  ),
+                  child: Image.network(
+                    menuImage,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 7, right: 7, top: 3),
-              child: Text(
-                menuTitle,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              Padding(
+                padding: EdgeInsets.only(left: 7, right: 7, top: 3),
+                child: Text(
+                  menuTitle,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 7,
+                  right: 7,
+                ),
+                child: Text(
+                  menuRestaurantTitle,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w100,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 7,
+                  right: 7,
+                ),
+                child: Row(
+                  textBaseline: TextBaseline.alphabetic,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  children: [
+                    Text(
+                      "฿ ${menuPrice.toStringAsFixed(0)}",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).accentColor,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "฿ ${_discount.toStringAsFixed(0)}",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
